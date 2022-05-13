7f8ee4a917dc5617d20b668f6f86bef826ecfa14
leam <marchaleo1410@gmail.com>
2022-05-13 14:37:33 +0200

    ✨ Add Questions

--- src/screen/QuestionScreen.jsx
+++ src/screen/QuestionScreen.jsx
@@ -1,14 +1,66 @@
 import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
 
-export function QuestionScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Question n°X</Text>
-        <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, amet.</Text>
-        <Button
-          title="Voir la reponse"
-          onPress={() => navigation.navigate('Response')}
-        />
-      </View>
-    );
+export function QuestionScreen({ route, navigation }) {
+  const {typeQuestion} = route.params;
+  //on recupère la valeur cliquée dans la page quizz, et on la définit en tant que route
+
+  switch (typeQuestion){//on passe la valeur récupérée de la  page quizz
+    case('ring') :
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Vite !</Text>
+          <Text>Répondez le plus rapidement possible !</Text>
+          <Button
+            title="Voir la reponse"
+            onPress={() => navigation.navigate('Response')}
+          />
+        </View>
+      );
+
+    case('check') :
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Concertation</Text>
+          <Text>Réflechissez en équipe !</Text>
+          <Button
+            title="Voir la reponse"
+            onPress={() => navigation.navigate('Response')}
+          />
+        </View>
+      );
+
+    case('nugget') :
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>x2</Text>
+          <Text>les points comptent doubles !</Text>
+          <Button
+            title="Voir la reponse"
+            onPress={() => navigation.navigate('Response')}
+          />
+        </View>
+      );
+
+    case('replay') :
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Replay</Text>
+          <Text>L'équipe rejoue</Text>
+          <Button
+            title="Retourner aux questions"
+            onPress={() => navigation.navigate('Quizz')}
+          />
+        </View>
+      );
+    default :
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>pas de question</Text>
+          <Button
+            title="Retourner sur le quizz"
+            onPress={() => navigation.navigate('Quizz')}
+          />
+        </View>
+      );
+  }
   }
--- src/screen/QuizzScreen.jsx
+++ src/screen/QuizzScreen.jsx
@@ -1,43 +1,99 @@
-import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, Pressable } from 'react-native';
 import { TeamContext } from '../context/context';
 import { useContext } from 'react';
 
 export function QuizzScreen({ navigation }) {
   const {team, setTeam} = useContext(TeamContext);
 
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Choisissez une question :</Text>
-        <Button
-          title="Question 1"
-          onPress={() => navigation.navigate('Question')}
-        />
-        <Button
-          title="Question 2"
-          onPress={() => navigation.navigate('Question')}
-        />
-        <Button
-          title="Question 3"
-          onPress={() => navigation.navigate('Question')}
-        />
-        <Button
-          title="Question 4"
-          onPress={() => navigation.navigate('Question')}
-        />
+  const questionNavigate = (value) => {//on récupère la valeur du pressable
+    navigation.navigate('Question', {
+      typeQuestion : value,
+    })
+  }
 
-        <Button
-          title="Fin du jeu"
-          onPress={() => navigation.navigate('Home')}
-        />
-        <View>
-          { team.map(team => {
-            return (
-              <View key={team.id}>
-                <Text>{team.TeamName} : {team.score}</Text>
-              </View>
-            )
-          }) }
+    return (
+      <View style={styles.container}>
+        <View style={styles.ViewTimer}>
+          <Text style={styles.ViewTimerText}>ScoreBoard</Text>
+          <Text style={styles.ViewTimerText}>Timer</Text>
         </View>
+        <View style={styles.ViewTeamScore}>
+            { team.map(team => {
+              return (
+                <View style={styles.ViewTeamScoreCard} key={team.id}>
+                  <Text style={styles.ViewTeamScoreCardText}>{team.TeamName}</Text>
+                  <Text>{team.score} points</Text>
+                </View>
+              )
+            }) }
+        </View>
+        <View style={styles.ViewTheme}>
+            <Pressable style={styles.ThemeCard} onPress={() => questionNavigate('ring')}>
+              <Text style={styles.ThemeCardText}>1</Text>
+            </Pressable>
+            <Pressable style={styles.ThemeCard} onPress={() => questionNavigate('check')}>
+              <Text style={styles.ThemeCardText}>2</Text>
+            </Pressable>
+            <Pressable style={styles.ThemeCard} onPress={() => questionNavigate('nugget')}>
+              <Text style={styles.ThemeCardText}>3</Text>
+            </Pressable>
+            <Pressable style={styles.ThemeCard} onPress={() => questionNavigate('replay')}>
+              <Text style={styles.ThemeCardText}>4</Text>
+            </Pressable>
+            <Pressable style={styles.ThemeCard} onPress={() =>questionNavigate()}>
+              <Text style={styles.ThemeCardText}>Bonus</Text>
+            </Pressable>
+          </View>
       </View>
     );
   }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1, 
+      alignItems: 'center', 
+      justifyContent: 'center',
+    },
+    ViewTimer: {
+      width: '100%',
+      height: '15%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingTop: 50,
+    },
+    ViewTeamScore: {
+      width: '100%',
+      height: '35%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'row',
+    },
+      ViewTeamScoreCard: {
+        width: '40%',
+        height: '20%',
+        backgroundColor: '#c7f0e3',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 5,
+      },
+      ViewTeamScoreCardText: {
+        fontSize: 16,
+      },
+    ViewTheme: {
+      width: '100%',
+      height: '55%',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+    },
+      ThemeCard: {
+        width: '80%',
+        height: '10%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        backgroundColor: '#FF7F00',
+      },
+      ThemeCardText: {
+        color: '#ffffff',
+      },
+})
